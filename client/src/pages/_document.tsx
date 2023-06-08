4918cabbfb174e2a6f35163cec1562569ef9d10a
Dazy888 <8205414@gmail.com>
2023-06-08 13:51:18 -0300

    Edited _document.tsx

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,13 +4,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => (
     <Html lang="en">
         <Head>
+            <link rel={'stylesheet'} href={'/fontawesome/css/all.css'}/>
             <link rel="icon" href="https://lh3.googleusercontent.com/DFc-owjHo7vlwlnS77oHER5DeLcY3YZtA6p1FSnVqXn4hVktbUdtnpc09lCHaBX0MDnl" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
         <Main />
         <NextScript />
-        <script src="https://kit.fontawesome.com/0a15c952b0.js" crossOrigin="anonymous" data-nscript="afterInteractive"/>
         </body>
     </Html>
 )
