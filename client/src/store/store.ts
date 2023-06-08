83943875c83d2e0618eafe0b1a17d34578b76e75
Dazy888 <8205414@gmail.com>
2023-06-08 14:21:22 -0300

    Edited store.ts

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,13 +2,8 @@ import { combineReducers } from "redux"
 import { configureStore } from "@reduxjs/toolkit"
 import { testReducer } from "@/store/reducer/TestSlice"
 
-const rootReducer = combineReducers({
-    testReducer
-})
+const rootReducer = combineReducers({ testReducer })
 
-export const store = configureStore({
-    reducer: rootReducer
-})
-
+export const store = configureStore({ reducer: rootReducer })
 export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
