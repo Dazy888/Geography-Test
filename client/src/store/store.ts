c1882ade36aeb7551008316bd28dde038cb2ff78
Dazy888 <8205414@gmail.com>
2023-06-08 14:27:39 -0300

    Reinitialized next.js app

--- client/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux"
-import { configureStore } from "@reduxjs/toolkit"
-import { testReducer } from "@/store/reducer/TestSlice"
-
-const rootReducer = combineReducers({ testReducer })
-
-export const store = configureStore({ reducer: rootReducer })
-export type RootState = ReturnType<typeof rootReducer>
-export type AppDispatch = typeof store.dispatch
