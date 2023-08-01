8a686339c8e5f5366204e0388a476e9f9141fbc0
Davyd Hutsenko <davydhutsenko@Sergiis-MacBook-Air.local>
2023-08-01 08:14:03 +0300

    Added manifest.json(PWA)

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,7 +5,8 @@ const Document = () => (
     <Html lang="en">
         <Head>
             <link rel={'stylesheet'} href={'/fontawesome/css/all.css'}/>
-            <link rel={'icon'} href={'favicon.png'} />
+            <link rel={'icon'} href={'/favicon.png'} />
+            <link rel={'manifest'} href={'/manifest.json'} />
             <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
         </Head>
         <body>
