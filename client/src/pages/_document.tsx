13204dc7b7e585c7aaa8b5a931ccd61cace62867
Davyd Hutsenko <davydhutsenko@Sergiis-MacBook-Air.local>
2023-08-01 08:36:39 +0300

    Created manifest directory in the public

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => (
         <Head>
             <link rel={'stylesheet'} href={'/fontawesome/css/all.css'}/>
             <link rel={'icon'} href={'/favicon.png'} />
-            <link rel={'manifest'} href={'/manifest.json'} />
+            <link rel={'manifest'} href={'/manifest/manifest.json'} />
             <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
         </Head>
         <body>
